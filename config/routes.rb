804c90e801cee75a0c8ee35c2fd616912f28f2c4
3rarara <161809435+3rarara@users.noreply.github.com>
2024-04-29 07:16:52 +0000

    [Finish]kadai6

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:show,:index,:create,:edit,:update,:destroy]
-  resources :users, only: [:show,:index,:edit,:update]
+  resources :books, only: [:show,:index,:create,:edit,:update,:destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+      get "followings" => "relationships#followings", as: "followings"
+      get "followers" => "relationships#followers", as: "followers"
+  end
 
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end

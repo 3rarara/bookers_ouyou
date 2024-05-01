17058c180bea79c3a3cca65878b642aa67a4f96a
3rarara <161809435+3rarara@users.noreply.github.com>
2024-05-01 04:40:57 +0000

    [finish]ouyou-a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
     @book_new = Book.new
     @book_comment = BookComment.new
   end
@@ -11,7 +14,6 @@ class BooksController < ApplicationController
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
     @books = Book.includes(:favorites).sort_by { |book| -book.favorites.where(created_at: from...to).count }
-
     @book = Book.new
   end
 

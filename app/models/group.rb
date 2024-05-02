fbbf1b09d46ba2d0568f4fb474eb8b16844e6026
3rarara <161809435+3rarara@users.noreply.github.com>
2024-05-02 00:56:04 +0000

    [Finish]ouyou-8c

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
 
   has_many :group_users, dependent: :destroy
   belongs_to :owner, class_name: 'User'
-  has_many :users
+  has_many :users, through: :group_users, source: :user
 
   validates :name, presence: true
   validates :introduction, presence: true
@@ -12,4 +12,9 @@ class Group < ApplicationRecord
   def is_owned_by?(user)
     owner.id == user.id
   end
+
+  def includesUser?(user)
+    group_users.exists?(user_id: user.id)
+  end
+  
 end
